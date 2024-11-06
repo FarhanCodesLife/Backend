3902b1271ef37b1839d84be100a9eea8919f40cf
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-11-07 00:16:34 +0500

    more

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-import express from 'express'
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-
-})
-
-app.get('about', (req, res) => {
-    res.status(404).send('Not Found')    
-  })
-  
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+l
