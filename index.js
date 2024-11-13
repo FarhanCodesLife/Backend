b041394b55b8ed3aa952e9a6772913b4d914109c
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-11-14 01:56:11 +0500

    all ok

--- index.js
+++ index.js
@@ -2,11 +2,50 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// midlewear 
+app.use(express.json())
 
+const user  = []
+
+app.post('/users', (req, res) => {
+  const {title} = req.body()
+
+  // age user ka title na ai to ya condition follow ho.
+  if(!title){
+    res.status(400).json({
+      message : 'title is not requird'
+    })
+    return;
+  }
+
+  // user ma data add kra na ka liya
+  user.push({
+    title : title
+  })
+
+  // responce bhajna ka liya 
+  res.status(201).json({
+    message : 'data add succesfully',
+    data : user
+  })
+})
+
+
+
+// req.param()
+// id get krna k a liya 
+// id ka zrya api create hogi 
+
+
+// thunderclint
+
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
   
+// app.get('/about', (req, res) => {
+//   res.send('Hello farhan!')
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
