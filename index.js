081606ec56facb8546345f59454f5a6a8615cd62
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-11-15 00:43:49 +0500

    start

--- index.js
+++ index.js
@@ -1,52 +1,20 @@
-import express from "express";
+import express from 'express'
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
-const user = [
-  {
-    data: {
-      id: 1,
-      name: "farhan",
-      email: "farhan@example.com",
-    },
-  },
-  {
-    data: {
-      id: 2,
-      name: "farooq",
-      email: "farooq@example.com",
-    },
-  },
-]
 app.get('/',(req,res)=>{
   res.send('hello world')
-  
-})
-app.post('/user',(req,res)=>{
-  const{name,id,email}=req.body;
-  res.status(201).json({
-   user
-  })
-  user.push({data:{id,name,email}})
-})
-app.get('/user/:id',(req,res)=>{
-  const {id} = req.params
-  const index = user.findIndex((item)=>item.data.id === +id)
-
-  if (index === -1) {
-    res.status(404).json({
-      message: "User not found",
-    });
-    return
-  }
-  
-  res.status(200).json({
-    data:user[index].data
-  })
 
 })
+
+
+app.get('/about',(req,res)=>{
+  res.send('hello about')
+  
+})
+
 
 
 
