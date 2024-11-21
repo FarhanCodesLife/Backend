874900f1a9e927239e261407bee8bf250279c6ff
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-11-22 00:27:20 +0500

    workin on

--- Mongoos/index.js
+++ Mongoos/index.js
@@ -6,15 +6,29 @@ import Routes from "./src/routes/todos.routes.js";
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 
+// Test route
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// routes
+// API routes
 app.use("/api/v1", Routes);
 
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
+// Fallback for undefined routes
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// Connect to MongoDB and start server
 connectDB()
   .then(() => {
     app.listen(process.env.PORT, () => {
