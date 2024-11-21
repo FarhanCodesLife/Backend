122283ef861ba855dd6d3a0d4de4683833b93cee
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-11-22 01:41:37 +0500

    all ok

--- Mongoos/src/models/todos.models.js
+++ Mongoos/src/models/todos.models.js
@@ -1,21 +1,62 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const todoSchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
+// const todoSchema = new Schema(
+//   {
+//     title: {
+//       type: String,
+//       required: true,
+//     },
+//     description: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// export default mongoose.model("Todos", todoSchema);
+
+
+
+
+import mongoose, { Schema } from "mongoose";
+
+const schema = mongoose.Schema;
+
+const todoschema = new schema({
+    title:{
+        type:String,
+        required:true,
+
     },
-    description: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+    description:{
+        type:string,
+        required:true,
 
-export default mongoose.model("Todos", todoSchema);
+    }
+},{
+    timestamps:true
+}
+
+
+)
+
+
+export default mongoose.model('Todo',todoschema)
+
+
+
+
+
+
+
+
+
+
+
+
+
