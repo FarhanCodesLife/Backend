c982f5eb060a77b92bb10222128ae2310d636012
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-12-02 23:52:12 +0500

    njnjn

--- authentication/src/controllers/users.controllers.js
+++ authentication/src/controllers/users.controllers.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken'
 import Users from '../models/users.modules.js'
 
-const generateAccessToken = (users) =>{ 
-    return jwt.sign({ email: users.email }, "farhan" , {expiresIn: '6h'});
+const generateAccessToken = (user) =>{ 
+    return jwt.sign({ email: user.email }, "farhan" , {expiresIn: '6h'});
 }
 const generateRefreshToken = (user) =>{ 
     return jwt.sign({ email: user.email }, "03182127256" , {expiresIn: '7d'});
