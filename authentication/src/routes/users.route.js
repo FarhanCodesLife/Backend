1e9acb238e16abc86bc56720ab43251d20a7dfd0
MUHAMMADFARHANHANIF <farhansmit0318@gmail.com>
2024-12-03 00:21:59 +0500

    ok

--- authentication/src/routes/users.route.js
+++ authentication/src/routes/users.route.js
@@ -1,11 +1,11 @@
 
 import mongoose from "mongoose";
-import { register } from "../controllers/users.controllers.js";
+import { loginUser, register } from "../controllers/users.controllers.js";
 import express from "express"
 const router = express.Router()
 
 router.post('/register',register)
-// router.post('/register',register)
+router.post('/login',loginUser)
 
 export default router
 
